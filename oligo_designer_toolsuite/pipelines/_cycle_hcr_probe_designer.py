909e03b620df8599735eb1b95bdf5031d0ef42f1
lisa-sousa <lisa.barros.andrade.sousa@gmail.com>
2025-11-27 20:04:55 +0100

    Enhance oligo sequence probe designer configuration and pipeline functionality
    - Added new parameters for k-mer abundance filtering and prohibited sequences in `oligo_seq_probe_designer.yaml` to improve oligo selection criteria.
    - Refactored the `OligoSeqProbeDesigner` class to incorporate the new prohibited sequences parameter, ensuring oligos containing specified sequences are filtered out.
    - Updated the main function to include logic for adding highly abundant k-mers to the prohibited sequences list, enhancing the specificity of the probe design process.
    - Replaced direct melting temperature parameter preprocessing with a dedicated function for improved clarity and maintainability across multiple probe designer pipelines.

--- oligo_designer_toolsuite/pipelines/_cycle_hcr_probe_designer.py
+++ oligo_designer_toolsuite/pipelines/_cycle_hcr_probe_designer.py
@@ -12,7 +12,6 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import yaml
-from Bio.SeqUtils import MeltingTemp as mt
 from Bio.SeqUtils import Seq
 
 from oligo_designer_toolsuite._exceptions import (
@@ -65,6 +64,7 @@ from oligo_designer_toolsuite.pipelines._utils import (
     check_content_oligo_database,
     format_sequence,
     pipeline_step_basic,
+    preprocess_tm_parameters,
     setup_logging,
 )
 from oligo_designer_toolsuite.sequence_generator import OligoSequenceGenerator
@@ -1993,13 +1993,6 @@ def main() -> None:
             # ensure that the list contains unique gene ids
             gene_ids = list(set([line.rstrip() for line in lines]))
 
-    ##### preprocess melting temperature params #####
-    target_probe_Tm_parameters = config["target_probe_Tm_parameters"]
-    target_probe_Tm_parameters["nn_table"] = getattr(mt, target_probe_Tm_parameters["nn_table"])
-    target_probe_Tm_parameters["tmm_table"] = getattr(mt, target_probe_Tm_parameters["tmm_table"])
-    target_probe_Tm_parameters["imm_table"] = getattr(mt, target_probe_Tm_parameters["imm_table"])
-    target_probe_Tm_parameters["de_table"] = getattr(mt, target_probe_Tm_parameters["de_table"])
-
     ##### initialize probe designer pipeline #####
     pipeline = CycleHCRProbeDesigner(
         dir_output=config["dir_output"],
@@ -2028,7 +2021,7 @@ def main() -> None:
             "target_probe_secondary_structures_threshold_deltaG"
         ],
         # Melting Temperature Calculation Parameters
-        target_probe_Tm_parameters=target_probe_Tm_parameters,
+        target_probe_Tm_parameters=preprocess_tm_parameters(config["target_probe_Tm_parameters"]),
         target_probe_Tm_chem_correction_parameters=config["target_probe_Tm_chem_correction_parameters"],
         target_probe_Tm_salt_correction_parameters=config["target_probe_Tm_salt_correction_parameters"],
         # Specificity Filter Parameters
