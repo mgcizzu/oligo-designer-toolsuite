ed5feb645599f680369440d6120f8dd0900cccfd
lisa-sousa <lisa.barros.andrade.sousa@gmail.com>
2026-02-10 16:55:41 +0100

    Reorder self_comp filter in TargetProbeDesigner: Move self_comp filter to the end of the filters list for improved clarity and maintainability in oligo design filtering logic.

--- oligo_designer_toolsuite/pipelines/_oligo_seq_probe_designer.py
+++ oligo_designer_toolsuite/pipelines/_oligo_seq_probe_designer.py
@@ -827,10 +827,10 @@ class TargetProbeDesigner:
         filters = [
             hard_masked_sequences,
             soft_masked_sequences,
-            self_comp,
             prohibited_sequence_filter,
             homopolymeric_runs,
             gc_content,
+            self_comp,
             melting_temperature,
             secondary_sctructure,
         ]
